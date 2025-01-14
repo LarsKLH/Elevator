dc8810583127bb5e8c84e9f26b409032b29a74ca
JensJahrenLauvrud <jensjl@stud.ntnu.no>
2025-01-14 18:02:06 +0100

    added rust bounded buffer

--- exercises/Exercise1-master/5 - bounded buffer/rust/bounded_buffer/src/main.rs
+++ exercises/Exercise1-master/5 - bounded buffer/rust/bounded_buffer/src/main.rs
@@ -1,3 +1,40 @@
+use std::{sync::mpsc, thread, thread::sleep, time::{self, Duration}};
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let  prod_sleep_time: Duration = time::Duration::from_millis(400);
+    let con_sleep_time: Duration = time::Duration::from_millis(600);
+
+    let (tx, rx) = mpsc::sync_channel(5);
+
+    let producer = thread::spawn(move || {
+        for i in 0 .. 10 {
+            sleep(prod_sleep_time);
+            println!("[producer] sending: {}", i);
+            tx.send(i).unwrap();
+        }
+    });
+
+    let consumer = thread::spawn(move ||{
+        loop {
+            sleep(con_sleep_time);
+            let i = rx.recv();
+
+            match i {
+                Ok(i) => println!("[consumer] recieving: {}", i),
+                Err(_) => break,
+            }
+            
+        }
+    });
+
+    producer.join().unwrap();
+    consumer.join().unwrap();
+
+
+
+
+    println!("Prog end clean!");
 }
---
+++ exercises/Exercise1-master/5 - bounded buffer/rust/bounded_buffer/target/debug/deps/bounded_buffer-3a53b979d950c271.d
@@ -0,0 +1,5 @@
+/home/gia2/yuuuuge_programs/yuge_stud/sanntidsprogramering/git_repo_dir/TTK4515-2025-Gruppe26/exercises/Exercise1-master/5 - bounded buffer/rust/bounded_buffer/target/debug/deps/bounded_buffer-3a53b979d950c271: src/main.rs
+
+/home/gia2/yuuuuge_programs/yuge_stud/sanntidsprogramering/git_repo_dir/TTK4515-2025-Gruppe26/exercises/Exercise1-master/5 - bounded buffer/rust/bounded_buffer/target/debug/deps/bounded_buffer-3a53b979d950c271.d: src/main.rs
+
+src/main.rs:
