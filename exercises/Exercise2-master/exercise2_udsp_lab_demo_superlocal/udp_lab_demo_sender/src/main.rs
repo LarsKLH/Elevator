b63d574ebfbf1929531ae49ed20ace8daf9ef27d
JensJahrenLauvrud <jensjl@stud.ntnu.no>
2025-01-21 18:24:12 +0100

    pushing the safty

--- exercises/Exercise2-master/exercise2_udsp_lab_demo_superlocal/udp_lab_demo_sender/src/main.rs
+++ exercises/Exercise2-master/exercise2_udsp_lab_demo_superlocal/udp_lab_demo_sender/src/main.rs
@@ -1,24 +1,29 @@
 use std::{str, net::UdpSocket,thread::sleep, time::Duration};
 
-const chosen_ip_sock: &str = "0.0.0.0:30005";
+const chosen_ip_sock: &str = "127.255.255.255:30005";
 
 const snedr_sleep_time: Duration = Duration::from_millis(300);
 
 
 
 fn main() {
+
     println!("Hello, world!");
 
     let socket_sender = UdpSocket::bind(chosen_ip_sock).unwrap();
+
+    socket_sender.set_broadcast(true).unwrap();
+
+    println!("Broadcast: {}", socket_sender.broadcast().unwrap());
 
     loop {
         let buf_sendr = "Message from sending program".as_bytes();
 
         println!("Sending from thred");
-        socket_sender.send_to(buf_sendr,chosen_ip_sock).expect("couldn't send data");
+        socket_sender.send_to(buf_sendr, chosen_ip_sock).expect("couldn't send data");
 
         let buf_sendr = "Sending a second message".as_bytes();
-        socket_sender.send_to(buf_sendr,chosen_ip_sock).expect("couldn't send data");
+        socket_sender.send_to(buf_sendr, chosen_ip_sock).expect("couldn't send data");
 
         sleep(snedr_sleep_time);
     }
