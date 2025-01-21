1481f262d967b6b2813db94721b6b25bc922b810
JensJahrenLauvrud <jensjl@stud.ntnu.no>
2025-01-21 17:00:18 +0100

    deleted the rest target filed

--- exercises/Exercise2-master/ex2_onlab_testing/target/debug/deps/ex2_onlab_testing-6e785d3786e079f2.d
+++
@@ -1,5 +0,0 @@
-/home/gia2/yuuuuge_programs/yuge_stud/sanntidsprogramering/git_repo_dir/TTK4515-2025-Gruppe26/exercises/Exercise2-master/ex2_onlab_testing/target/debug/deps/libex2_onlab_testing-6e785d3786e079f2.rmeta: src/main.rs
-
-/home/gia2/yuuuuge_programs/yuge_stud/sanntidsprogramering/git_repo_dir/TTK4515-2025-Gruppe26/exercises/Exercise2-master/ex2_onlab_testing/target/debug/deps/ex2_onlab_testing-6e785d3786e079f2.d: src/main.rs
-
-src/main.rs:
