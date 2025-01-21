fc1b0c5a2755f089dfd08b6689bbfee168a7ab2a
LarsKr <lars.lie.hauge@gmail.com>
2025-01-21 18:35:14 +0100

    Written tcp connection file done

--- exercises/Exercise2-master/exercise2_tcp/src/main.rs
+++ exercises/Exercise2-master/exercise2_tcp/src/main.rs
@@ -1,7 +1,9 @@
 use std::net::{TcpStream, TcpListener};
 use std::io::{Write, Read};
+use std::thread::sleep;
 
 fn handle_stream(mut stream: TcpStream){
+    loop{
     // Create a buffer of 1024 bytes to hold data
     let mut buffer = [0; 1024];
 
@@ -11,22 +13,36 @@ fn handle_stream(mut stream: TcpStream){
     // Print the data from the buffer in readable format
     let received_data = String::from_utf8_lossy(&buffer);
     println!("Data received: {}", received_data);
+    sleep(std::time::Duration::from_millis(1000));
 
     // Write response to the stream in correct format
-    let response = "Hello from the listener!".as_bytes();
-    stream.write(response).expect("Couldn't write data to the stream...");
+    //let response = "Hello from the listener!".as_bytes();
+    //stream.write(response).expect("Couldn't write data to the stream...");
+}
 
 }
 
 fn main() {
     // Bind the listener to the local address
     let listener = TcpListener::bind("127.0.0.1:80").expect("Couldn't bind to the address...");
+    let mut sender = TcpStream::connect("127.0.0.1:80").expect("Couldn't connect to the server...");
+    
+    
+    std::thread::spawn(move || {
+        for i in 0 .. 10 {
+            let message = format!("Message number: {}\n", i);
+            sender.write(message.as_bytes()).expect("Couldn't write data to the listener...");
+            println!("[Stream] sending: {}", message);
+        }
+    });
 
     //let mut stream = TcpStream::connect("127.0.0.1:34254")?;
+    
     for stream in listener.incoming(){
         match stream{
             Ok(stream) => {
-                std::thread::spawn(|| {handle_stream(stream);});
+                std::thread::spawn(move ||
+                    {handle_stream(stream);});
                 }
             Err(e) => {
                 eprintln!("Error: {}", e);
