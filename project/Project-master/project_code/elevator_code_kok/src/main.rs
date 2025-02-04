2da08afea4e87f377cd588885640873215e26dc8
JensJahrenLauvrud <jensjl@stud.ntnu.no>
2025-02-04 19:11:37 +0100

    added project kok to fugrd

--- project/Project-master/project_code/elevator_code_kok/src/main.rs
+++ project/Project-master/project_code/elevator_code_kok/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+
+
+
+use driver_rust::elevio;
+
+
+
+
+
+fn main() -> std::io::Result<()> {
+    let num_floors = 4;
+    let elevator = elevio::elev::Elevator::init("localhost:15657", num_floors)?;
+    Result::Ok(())
 }
