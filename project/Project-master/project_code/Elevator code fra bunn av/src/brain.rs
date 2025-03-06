eca57c6e9d291a87d7288241adf632223e95911f
Lars Kristian <lars.lie.hauge@gmail.com>
2025-03-06 15:17:36 +0100

    Updated brain after Type changes

--- project/Project-master/project_code/Elevator code fra bunn av/src/brain.rs
+++ project/Project-master/project_code/Elevator code fra bunn av/src/brain.rs
@@ -12,6 +12,8 @@ use crate::elevator_interface::{self as elevint, Direction};
 
 use driver_rust::elevio::{self, elev::{self, Elevator}};
 
+// The symbol ¤ is used where the code is not yet implemented and needs to be done later, or i have questions about the code
+
 
 // The main elevator logic. Determines where to go next and sends commands to the motor controller
 pub fn elevator_logic(memory_request_tx: Sender<mem::MemoryMessage>, memory_recieve_rx: Receiver<mem::Memory>, floor_sensor_rx: Receiver<u8>) -> () {
@@ -51,19 +53,23 @@ pub fn elevator_logic(memory_request_tx: Sender<mem::MemoryMessage>, memory_reci
             } 
             elevint::MovementState::StopDoorClosed => {
                 println!("Stopping and closing door");
-                //  Determine next direction using should_i_go and send to memory
+                //  ¤Determine next direction using should_i_go and send to memory
             }
             elevint::MovementState::StopAndOpen => {
                 println!("Stopping and opening door");
-                // Change callstate to PendingRemoval in memory
-                // Determine next direction using should_i_go and send to memory
+                // ¤Change callstate to PendingRemoval in memory
+                // ¤Determine next direction using should_i_go and send to memory
             }
 
-            // Need to add a case for the Obstructed state that 
-            // dont allow for ANY movement until the obstruction is removed
-            // Questions for later: Should the elevator remember the last direction it was moving in; Can obstructions occur both when
-            // in motion and when standing still (with or without open doors);
-            // See elevint file for clariity??
+            elevint::MovementState::Obstructed => {
+                println!("Elevator is obstructed");
+                // ¤Determine next direction using should_i_go and send to memory         
+                // dont allow for ANY movement until the obstruction is removed
+
+            }
+
+            // ¤Questions for later: Should the elevator remember the last direction it was moving in?
+            // ¤Can obstructions occur both when in motion and when standing still (with or without open doors)?
 
         }
     }
@@ -71,91 +77,91 @@ pub fn elevator_logic(memory_request_tx: Sender<mem::MemoryMessage>, memory_reci
 
 // Check whether we should stop or not
 fn should_i_stop(new_floor: u8, my_state: &mem::State) -> bool {
-        
+
+    let calls: Vec<_> = my_state.call_list.clone().into_iter().collect(); // Store call_list as a vec for future filtering    
     let my_floor = new_floor;
-    let my_direction = my_state.move_state
+    let my_direction: elevint::Direction = match my_state.move_state {
+        elevint::MovementState::Moving(dirn) => dirn,
+        _ => {                                                            // This should never happen
+            println!("Error: Elevator is not moving. Defaulting to Up."); 
+            elevint::Direction::Up                                        // Provide a fallback value
+        }
+    };
 
-    let calls: Vec<_> = my_state.call_list.clone().into_iter().collect(); // Store in a Vec
 
+    // Check if my current floor is confirmed using filter -> stop
     let my_call_is_confirmed = calls.iter()
         .any(|(call, state)| *state == mem::CallState::Confirmed && call.floor == my_floor);
     
     if my_call_is_confirmed {
         return true;
     }
+
     
+    // Check if there are no confirmed floors in the direction of the elevator -> stop
     let no_confirmed_calls_in_direction = calls.iter()
         .filter(|(call, state)| *state == mem::CallState::Confirmed) // Keep only confirmed calls
-        .all(|(call, _)| match my_direction {
-            mem::Direction::Up => call.floor <= my_floor, // No confirmed calls above
-            mem::Direction::Down => call.floor >= my_floor, // No confirmed calls below
+        .all(|(call, _)| match my_direction {                   // ¤should maybe use .any() instead of .all() here
+            elevint::Direction::Up => call.floor <= my_floor,
+            elevint::Direction::Down => call.floor >= my_floor,
         });
     
     if no_confirmed_calls_in_direction {
         return true; // Stop the elevator
     }
+
     // Else continue moving in current direction
+    return false;
 
-
-
-    // Move the if-statements bellow INSIDE the match statement if you can NOT access the check_call variable outside of the match statement
-    // elsewise you can keep it as it is
-    // If the state of our current floor is confirmed, we should stop
-    if *my_state.call_list.get(&check_call).unwrap() == mem::CallState::Confirmed {
-        return true;
-    }
-    // if there are no more confirmed floors below or above us, we should stop
-    else if !other_floors_confirmed(new_floor, my_state.clone()) {
-        return true;
-    }
-    else {
-        return false;
-    }
 }
 
-// Fix my_state.direction as it was done in the function above, or pass it as an argument
-fn other_floors_confirmed(new_floor: u8, my_state: mem::State) -> bool {
-    match my_state.move_state {
-        elevint::MovementState::Moving(elevint::Direction::Up)    => {
-            for call in my_state.call_list {
-                if call.0.floor > new_floor && call.1 == mem::CallState::Confirmed {
-                    return false;
-                }
-            }
-        }
-        elevint::MovementState::Moving(elevint::Direction::Down)    => {
-            for call in my_state.call_list {
-                if call.0.floor < new_floor && call.1 == mem::CallState::Confirmed {
-                    return false;
-                }
-            }
-        }
-        _ => {
-            println!("Error: Direction not valid");
-        }
-    }
-    return true
-}
 
 // should_i_go, checks if the elevator should go up or down or if another elevator should take the call
 fn should_i_go(my_state: mem::State) -> () {
+
+    // ¤This function needs to check both cab_calls and call_list (I advice cab_calls take president over call_list) 
+    // ¤for determining the next direction of the elevator
+    // ¤Also needs to check if another elevator is closer to the call than this elevator
+    // ¤May need to use the distance function from the memory.rs file ??
+    // ¤May need to take the direction of the elevator into account when checking if another elevator is closer and if 
+    // ¤the elevator is moving or not (and which floor is more advantageous to go to)
+
     println!("Checking if I should go");
-    // This function needs to check both cab_calls and call_list (I advice cab_calls take president over call_list) 
-    // for determining the next direction of the elevator
-    // Also needs to check if another elevator is closer to the call than this elevator
-    // May need to use the distance function from the memory.rs file ??
-    // May need to take the direction of the elevator into account when checking if another elevator is closer and if 
-    // the elevator is moving or not (and which floor is more advantageous to go to)
+    let calls: Vec<_> = my_state.call_list.clone().into_iter().collect(); // Store call_list as a vec for future filtering    
+    let my_floor = my_state.last_floor;
+    let my_movementstate = my_state.move_state;
+    let my_direction = match my_movementstate {
+        elevint::MovementState::Moving(dirn) => Some(dirn),
+        _ => {
+            println!("Error: Elevator is not moving.");
+            None
+        }
+    };
+    
+    // Check if elevator is obstructed, 
+    //¤or maybe this should be done in the elevator_logic function so that the elevator does not move at all
+    let is_obstructed = my_state.obstructed;
 
-    if other_floors_confirmed(my_state.last_floor, my_state.clone()) {
-        // If there are confirmed floors below or above us, we should maybe start moving
+    // Check if elevator holds any cab calls
+    let cab_calls = calls.iter()
+        .any(|(call, state)| call.call_type == mem::CallType::Cab && *state == mem::CallState::Confirmed);
 
+    let hall_calls = calls.iter()
+        .any(|(call, state)| call.call_type == mem::CallType::Hall && *state == mem::CallState::Confirmed);
+
+    if cab_calls {
+        // If there are cab calls, we should maybe start moving
+        // Move in the direction of most advantageous cab call
     }
-    else {
-        // If there are more confirmed floors below or above us, we should continue
-        memory_request_tx.send(mem::MemoryMessage::UpdateOwnMovementState(elevint::MovementState::Moving(my_state.direction))).unwrap();
+
+    if hall_calls {
+        // If there are hall calls, we should maybe start moving
+        // Move in the direction of most advantageous hall call
     }
 
+    
+
+    
 }
 
 
