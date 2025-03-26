92ce0239ac7588a316f24de148a44808638def54
Lars Kristian <lars.lie.hauge@gmail.com>
2025-03-26 09:57:56 +0100

    .

--- project/Project-master/project_code/Elevator code fra bunn av/src/brain.rs
+++ project/Project-master/project_code/Elevator code fra bunn av/src/brain.rs
@@ -313,7 +313,7 @@ fn should_i_go(current_dir: Direction, memory_request_tx: Sender<mem::MemoryMess
             }
             match calls_in_current_direction.is_empty() {
                 false => {
-                    clear_confirmed_calls_on_floor_matching_direction(my_state.clone(),  memory_request_tx.clone(), prev_direction);
+                    clear_confirmed_calls_on_floor_matching_direction(my_state.clone(),  memory_request_tx.clone(), current_dir);
                     println!("Brain: There are more calls in my current direction {:?} from before I stopped, continuing to move in that direction", current_dir);
                     memory_request_tx.send(mem::MemoryMessage::UpdateOwnMovementState(elevint::MovementState::Moving(current_dir))).expect("Error sending movement state to memory");
                     thread::sleep(Duration::from_millis(100));
@@ -324,14 +324,14 @@ fn should_i_go(current_dir: Direction, memory_request_tx: Sender<mem::MemoryMess
 
             match calls_in_opposite_direction.is_empty() {
                 false => {
-                    clear_confirmed_calls_on_floor_matching_direction(my_state.clone(),  memory_request_tx.clone(), prev_direction);
+                    clear_confirmed_calls_on_floor_matching_direction(my_state.clone(),  memory_request_tx.clone(), current_dir);
                     println!("Brain: There are no more hall calls in my previous direction {:?} from before I stopped but there are calls in the other direction, turning around to move in other direction", current_dir);
                     let current_dir = match current_dir {
                         Direction::Up => Direction::Down,
                         Direction::Down => Direction::Up,
                     };
                     memory_request_tx.send(mem::MemoryMessage::UpdateOwnMovementState(elevint::MovementState::Moving(current_dir))).expect("Error sending movement state to memory");
-                    clear_confirmed_calls_on_floor_matching_direction(my_state.clone(),  memory_request_tx.clone(), prev_direction);
+                    clear_confirmed_calls_on_floor_matching_direction(my_state.clone(),  memory_request_tx.clone(), current_dir);
                     thread::sleep(Duration::from_millis(100));
                     return true;
                 }
