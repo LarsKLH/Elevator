e99df9aa38aa3d9bf072c589c36f8d26ca0e8903
N0elurt <brynildsenwalter@gmail.com>
2025-03-28 00:06:23 +0100

    Fixed slight mistake

--- project/Project-master/project_code/Elevator code fra bunn av/src/sanity.rs
+++ project/Project-master/project_code/Elevator code fra bunn av/src/sanity.rs
@@ -420,7 +420,7 @@ fn deal_with_calls_for_other(received_memory: mem::Memory, old_memory: mem::Memo
     let mut calls_difference_assembled = hall_calls_difference.clone();
     calls_difference_assembled.extend(cab_calls_difference.clone());
 
-    let mut received_state_to_commit = received_memory.state_list.get(&received_memory.my_id).expect("Sanity: Wrong in state, cannot deal with it").clone();
+    let mut received_state_to_commit = old_memory.state_list.get(&received_memory.my_id).expect("Sanity: Wrong in state, cannot deal with it").clone();
 
     for change in calls_difference_assembled.clone() {
         received_state_to_commit.call_list.insert(change.0, change.1);
