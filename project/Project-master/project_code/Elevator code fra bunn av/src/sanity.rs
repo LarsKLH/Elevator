f4354dc4b206cb4e054b8eb6547812229c11d1a1
N0elurt <brynildsenwalter@gmail.com>
2025-03-27 06:42:29 +0100

    .

--- project/Project-master/project_code/Elevator code fra bunn av/src/sanity.rs
+++ project/Project-master/project_code/Elevator code fra bunn av/src/sanity.rs
@@ -379,6 +379,7 @@ fn deal_with_calls_for_other(received_memory: mem::Memory, old_memory: mem::Memo
     // Getting the old and received interpretations of our cab calls
     let mut cab_calls_for_comparison = HashMap::new();
     cab_calls_for_comparison.insert(old_memory.my_id,old_memory.state_list.get(&received_memory.my_id).expect("Sanity: Wrong in state, cannot deal with it").clone());
+    cab_calls_for_comparison.insert(received_memory.my_id,received_memory.state_list.get(&received_memory.my_id).expect("Sanity: Wrong in state, cannot deal with it").clone());
     let cab_calls_filtered = filter_changes(cab_calls.clone(), received_memory.state_list.get(&received_memory.my_id).expect("Sanity: Wrong in state, cannot deal with it").clone().last_floor, cab_calls_for_comparison.clone());
 
     let cab_calls_difference = difference(cab_calls.clone(), cab_calls_filtered.clone());
